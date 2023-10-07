0f5340acf8e2eb738ca51e4ed0c10446515a1016
itmarko <bhagwan13397@gmail.com>
2023-10-07 10:51:56 +0530

    first commit

---
+++ src/com/practice/dsa/ReverseArray.java
@@ -0,0 +1,30 @@
+package com.practice.dsa;
+
+public class ReverseArray {
+	
+	static void reverseArray(int arr[], int start, int end) {
+		  int temp;
+		  if(start>=end)
+			  return;
+		  temp= arr[start];
+		  arr[start] = arr[end];
+		  arr[end] = temp;
+		  
+		  reverseArray(arr, start+1, end-1);
+		
+	}
+	
+	static void PrintArray(int arr[], int n){
+		for(int i=0; i<n; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,6};
+		
+		reverseArray(arr,0,arr.length-1);
+		PrintArray(arr, arr.length);
+	}
+
+	
+}
